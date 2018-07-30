97a4fc36a0b16add582437536394f5c573f25ff6
Vince <vdidry@gmail.com>
2018-07-30 20:52:47 +0200

    Adding stores list on homepage and stores page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,7 +10,12 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-    const store = new Store(req.body);
-    await store.save();
-    res.redirect('/');
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review ?`);
+    res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+    const stores = await Store.find();
+    res.render('stores', {title: 'Stores', stores});
 };
