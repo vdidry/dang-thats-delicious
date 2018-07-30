37d99488009bd1eaaa9f6e1f5eb0b0c471ac6661
Vince <vdidry@gmail.com>
2018-07-30 21:32:50 +0200

    Add Store edition

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -6,7 +6,7 @@ exports.homePage = (req, res) => {
 };
 
 exports.addStore = (req, res) => {
-    res.render('editStore', { title: 'Add Store'});
+    res.render('editStore', {title: 'Add Store'});
 };
 
 exports.createStore = async (req, res) => {
@@ -18,4 +18,20 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
     const stores = await Store.find();
     res.render('stores', {title: 'Stores', stores});
+};
+
+exports.editStore = async (req, res) => {
+    const store = await Store.findOne({_id: req.params.id});
+    res.render('editStore', {title: `Edit ${store.name}`, store})
+};
+
+exports.updateStore = async (req, res) => {
+    const store = await Store.findOneAndUpdate(
+        {_id: req.params.id},
+        req.body,
+        {new: true, runValidators: true}
+    ).exec();
+
+    req.flash('success', `Successfully updates <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+    res.redirect(`/stores/${store._id}/edit`);
 };
