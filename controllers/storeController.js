fee42c6f77e7cdc201fdaebcbb92e5f81bf16b60
Vince <vdidry@gmail.com>
2018-07-31 22:37:04 +0200

    Add lat lng and address to stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -26,6 +26,7 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+    req.body.location.type = 'Point';
     const store = await Store.findOneAndUpdate(
         {_id: req.params.id},
         req.body,
