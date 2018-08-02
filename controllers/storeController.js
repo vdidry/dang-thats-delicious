36f1429cd63cd3d1ad1ec0864d00c506d11c7a94
Vince <vdidry@gmail.com>
2018-08-02 22:56:38 +0200

    Add single store page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -64,3 +64,11 @@ exports.updateStore = async (req, res) => {
     req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
     res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+    const store = await Store.findOne({slug: req.params.slug});
+    if(!store) {
+        return next();
+    }
+    res.render('store', {store, title: store.name});
+};
