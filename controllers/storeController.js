b33c07936d2c3361596dc262aef2749b0651e829
Vince <vdidry@gmail.com>
2018-09-02 21:56:13 +0200

    Add api endpoint store coordinates

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -104,3 +104,21 @@ exports.searchStores = async(req, res) => {
   .limit(5);
   res.json(stores);
 };
+
+exports.mapStores = async (req,res) => {
+    const coordinates = [req.query.lng, req.query.lat].map(parseFloat);
+    const q = {
+        location: {
+            $near: {
+                $geometry: {
+                    type: 'Point',
+                    coordinates: coordinates
+                },
+                $maxDistance: 10000
+            }
+        }
+    };
+
+    const stores = await Store.find(q).select('slug name description location').limit(10);
+    res.json(stores);
+};
