b99b6e506f04404d56def17056197f108ec6e802
Vince <vdidry@gmail.com>
2018-09-02 19:44:07 +0200

    Add ajax search

--- routes/index.js
+++ routes/index.js
@@ -46,4 +46,6 @@ router.post('/account/forgot', catchErrors(authController.forgot));
 router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account/reset/:token', authController.confirmPasswords, catchErrors(authController.update));
 
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;
